b4b963f26f21e5f5f00e33bb0812c044f2720860
iury <iury@iury-PC>
2018-12-19 00:52:38 -0300

    apagando o @Table(name="Tabela_Aluno")

--- src/br/unitins/unibio/model/Aluno.java
+++ src/br/unitins/unibio/model/Aluno.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Tabela_Aluno")
 public class Aluno extends Pessoa {
 
 	private static final long serialVersionUID = -3763771903748482697L;
